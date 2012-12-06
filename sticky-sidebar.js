b117853be487ce2cf4384e9c8c3a0a00bde57551
Brad Cooper <bradleyjcooper@gmail.com>
2012-12-06 17:11:49 -0500

    changed some of the comments to be more clear.

--- sticky-sidebar.js
+++ sticky-sidebar.js
@@ -7,27 +7,23 @@
  *
  * http://willworkforart.net/
  */
- 
+
 /*jslint white: true, onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true, strict: true, browser: true, windows: true */
 /*global window */
 function stickySidebar() {
 	"use strict";
 	var theSidebar, originalSidebarPosition;
 	theSidebar = document.getElementById("sidebar");
-
 	//If the Sidebar doesn't exist - kick out
 	if (!theSidebar) { return; }
-	
 	//Get the original position of the sidebar and keep for later
 	originalSidebarPosition = theSidebar.offsetTop;
-	
-	//Figure out how far the page has scrolled (not consistent between browsers) ...
+	//Figure out how far the page has scrolled (because different browsers do it differently) ...
 	function getScrollFromTop() {
 		var thePosition = window.pageYOffset;
 		if (thePosition === undefined) { thePosition = document.documentElement.scrollTop; }
 		return thePosition;
 	}
-	
 	//What to do when scrolling
 	window.onscroll = function () {
 		if (getScrollFromTop() <= originalSidebarPosition) {
@@ -35,7 +31,7 @@ function stickySidebar() {
 		} else {
 			theSidebar.style.position = "fixed";
 			theSidebar.style.top = "0px";
-		}				
+		}
 	};
 }
 window.onload = stickySidebar;
